c2344843446acff07f0fcbea0d838811e5611662
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-09 06:35:13 -0700

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
-// This is a print function example.
-
+/**
+* main -Entry point
+*
+*Return: Always 0 (success)
+*/
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
- }
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
