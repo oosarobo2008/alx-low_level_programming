dcc3d421aceec3f8f41b83cacb1dddd0a86c8893
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-09 16:25:09 -0700

    uy

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - make i count one two three like nursery school pikin
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int num;
+for (num = 0 ;num <= 10; num++)
+{
+putchar(num);
+}
+putchar(\n);
+return (0);
+}
