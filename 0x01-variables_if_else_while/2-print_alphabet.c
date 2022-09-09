536c7e037188a3d13010cdb22343e6b9d4e42558
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-09 14:36:14 -0700

    j

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+
+ * main - lower letters
+
+ * Return: always 0 (success)
+
+ */
+
+int main(void)
+  
+{
+  char henry = 'a';
+  
+  while (henry <= 'z')
+    
+    {
+      
+      putchar(henry);
+      
+      henry++;
+      
+    }
+  
+  putchar(10);
+  
+  return (0);
+  
+}
