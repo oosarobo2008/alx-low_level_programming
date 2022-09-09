dd9c45adceaa927039fed5ddcad1545e68477cb3
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-09 14:19:07 -0700

    h

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-  int final; 
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  final = n % 10;
+    int final; 
+    int n;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    final = n % 10;
   if (final > 5)
     {
       printf("Last digit of %d is %d and is greater than 5\n", n, final); 
@@ -22,7 +22,8 @@ int main(void)
       printf("Last digit of %d is %d and is less than 6 and not 0\n", n, final); 
     }
   else if (final == 0)
-    {printf("Last digit of %d is %d and is 0\n", n, final);
+    {
+      printf("Last digit of %d is %d and is 0\n", n, final);
     }
-  return (0);
+     return (0);
 }
