5ab7384cb3b3298c624af35656497c99256c1a4f
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-09 14:41:32 -0700

    h

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+  char henry = 'a'; 
+  char hamza = 'A';
+  for (henry <= 'a'; henry++)
+    {
+      if (henry <= 'z'){
+	
+	putchar(henry);
+	
+      }
+      
+      else {return (0)
+	  
+	  }
+      
+      while (hamza <= 'Z')
+	
+	{
+	  
+	  putchar(hamza);
+	  
+	  hamza++;
+	  
+	  putchar(10);
+	  
+	}
+      
+      return (0);
+      
+    }
