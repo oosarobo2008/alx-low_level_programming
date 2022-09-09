a4de95dbc29c8f86c1e39afd74d4354e7bf67917
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-09 07:05:21 -0700

    h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 /**
- *main - Entry point
- *Return:Always 1 (success)
- */
+*main - Entry point
+*Return:Always 1 (success)
+*/
 int main(void)
+
 {
-  char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, str1, 59);
-  return (1);
+
+ char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ write(2, str1, 59);
+ return (1);
 }
