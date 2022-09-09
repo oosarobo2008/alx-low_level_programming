a48488916c72eba09c5a28270811f617e968f852
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-09 06:38:52 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point
+* * main - Entry point
 *
+*Description: prints
 *Return: Always 0 (success)
 */
 int main(void)
-{
-puts("\" Programming is like building a multilingual puzzle");
+{ puts("\" Programming is like building a multilingual puzzle");
 return (0);
 }
