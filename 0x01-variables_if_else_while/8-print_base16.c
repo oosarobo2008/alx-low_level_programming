86eaa5be6c1f349c2293703a18a8aea56d6dd437
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-10 09:16:05 -0700

    k

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,43 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
-
- * main - hexadecimal wahala
-
- * Return always 0 (success)
-
- * Return: always 0 (success)
-
- */
+* main - hexadecimal number
+* Desciption of main function
+* printing of hexadecimal digit numbers
+* Return: always 0 (success)
+*/
 
 int main(void)
-  
 {
-  
-  int gun = 0;
-  
-
-  
-  while (gun < 48)
-    
-    {
-      
-      if (gun < 10 )
-	
-	if (gun < 10)
-	  
-	  putchar(gun + '0');
-      
-	else if (gun > 41)
-	  
-	  putchar(gun - 10 + 'A');
-      
-      gun++;
-      
-    }
-  
-  putchar(10);
-  
-  return (0);
-  
+int hex;
+char low;
+for (hex = '0'; hex <= '9'; hex++)
+putchar(hex);
+for (low  = 'a'; low <= 'f'; low ++)
+putchar(low);
+putchar('\n');
+return (0);
 }
