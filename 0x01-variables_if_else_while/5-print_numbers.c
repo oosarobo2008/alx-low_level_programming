074e027962347b3da1ce63dabbbb5f2e7ff3b3b6
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-10 03:07:54 -0700

    j

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,33 +1,18 @@
 #include <stdio.h>
 
 /**
-
- * main - count 1 2 3  , as par nursery svchool pikin wey i be na
-
- * Return: Always 0 (success)
-
- */
+* main - count 1 2 3  , as par nursery svchool pikin wey i be na
+* Return: Always 0 (success)
+*/
 
 int main(void)
-  
 {
-  
-  int pad;
-  
-
-  
-  for (pad = 0; pad <= 10; pad++)
-    
-    for (pad = 0; pad <= 9; pad++)
-      
-      {
-	
-	printf("%d", pad);
-	
-      }
-  
-  printf("\n");
-  
-  return (0);
-  
+int pay;
+for (pay = 0; pay <= 10; pay++)
+for (pay = 0; pay <= 9; pay++)
+{
+printf("%d", pay);
+}
+printf("\n");
+return (0);
 }
