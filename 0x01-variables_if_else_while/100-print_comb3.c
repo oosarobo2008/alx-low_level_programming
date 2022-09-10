d2ad496b5ff23547c349ae67cf2de352a4c02e14
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-10 03:46:13 -0700

    j

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,9 +2,7 @@
 
 /**
 * main - print single digit numbers
-*
 * Description: print two digit numbers with commas
-*
 * Return: Always 0 (Success)
 */
 
@@ -16,14 +14,14 @@ j = 48;
 while (i < 58)
 {
 j = 48;
- while (j < 58)
+while (j < 58)
 {
 putchar(i);
 putchar(j);
  
 if (i < 57 || j < 57)
 
-}
+{
 putchar(44);
 putchar(32);
 }
