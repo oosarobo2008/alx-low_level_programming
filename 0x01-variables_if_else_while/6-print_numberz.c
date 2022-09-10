69a841b852633ad88283d97b6af9cbfef62f8ed6
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-10 04:03:34 -0700

    j

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
 
 /**
-
- * main - make i count one two three like nursery school pikin
-
- * Return: Always 0 (success)
-
- */
+* main - print single digit of base 10
+* Return: Always 0 (success)
+*/
 
 int main(void)
-  
 {
-  
-  int m;
-  
-
-  
-  for (m = 38 ; m < 48 ; m++)
-    
-    for (m = 48 ; m < 58 ; m++)
-      
-      {
-	
-	putchar(m);
-	
-      }
-  
-  putchar('\n');
-  
-  return (0);
-  
+int m;
+for (m = 0 ; m < 10 ; m++)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
 }
