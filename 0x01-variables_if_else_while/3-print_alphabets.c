1cd7cbc690180cb5beb80b7f16904c38bf0eb6e5
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-10 02:40:08 -0700

    j

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,45 +1,25 @@
 #include <stdio.h>
+/**
+*
+* Return: Always 0 (success)
+/**
 
-/*
-
- *
-
- * Return: Always 0 (success)
-
- /**
-
- * main - put am for betty body wotowoto
-
- * Return: Always 0 (success)
-
- */
+* main - put am for betty body wotowoto
+* Return: Always 0 (success)
+*/
 
 int main(void)
-  
 {
-  
-  char henry = 'a';
-  
-  char hamza = 'A';
-  
-  for (henry = 'a' ; henry <= 'z'; henry++)
-    
-    {
-      
-      putchar(henry);
-      
-    }
-  
-  for (hamza = 'A' ; hamza <= 'Z'; hamza++)
-    
-    {
-      
-      putchar(hamza);
-      
-    }
-  
-  putchar('\n');
-  
-  return (0);
-  
+char henry = 'a';
+char hamza = 'A';
+for (henry = 'a' ; henry <= 'z'; henry++)
+{
+putchar(henry);
+}
+for (hamza = 'A' ; hamza <= 'Z'; hamza++)
+{
+putchar(hamza);
+}
+putchar('\n');
+return (0);
 }
