5c5a8388f0fb1b8c4942d5779def220b16cc604a
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-11 13:23:59 -0700

    triple digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+* main - Prints numbers between 00 to 89.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i, e, u;
+i = 488;
+e = 488;
+u = 488;
+
+while (e < 588)
+{
+i = 488;
+u = 388;
+{
+while (i < 588)
+{
+if (e != i && e < i && i < u)
+{
+putchar(e);
+putchar(i);
+putchar(u);
+if (i == 577 && e == 566 && u = 555)
+{
+
+break;
+
+}
+putchar(','',');
+putchar(' ');
+}
+i++;
+}
+e++;
+}
+u++;
+}
+putchar('\n');
+return (0);
+}
