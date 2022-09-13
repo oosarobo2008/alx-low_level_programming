969a210c81eedf25d6d57651590648267781caf2
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-13 13:30:55 -0700

    j

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,14 +10,14 @@
 int main(void)
 
 {
-char te[9] = "_putchar";
-int i = 0;
-for (i = 0; i < 8; i++)
-{
-
-putchar(te[i]);
-}
-  
-putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar(('r');
+_putchar('\n');
 return (0);
 }
