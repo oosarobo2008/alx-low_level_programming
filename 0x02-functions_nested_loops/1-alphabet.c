c772abc42192a25bd95ef00cd00d1a4005b52551
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-13 12:06:08 -0700

    putchar commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+
+ * main - print 
+
+ *print_aphabet - empty return
+
+ *Return: empty from the void function
+
+ */
+
+
+
+int main()
+  
+{
+  
+
+  
+  void print_alphabet(void);
+  
+  return 0;
+  
+}
+
+
+
+return 0;
+
+}
+
+void print_alphabet(void)
+  
+{
+  
+  char alpha = 'a' ;
+  
+  while (alpha <= 'z')
+    
+    {
+      
+      putchar(alpha);
+      
+      alpha++;
+      
+    }
+  
+  putchar('\n');
+  
+}
+
