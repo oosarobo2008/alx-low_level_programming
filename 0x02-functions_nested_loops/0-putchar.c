9f31c8db5e4872ed2a6810258afd51d51602c71a
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-13 11:43:55 -0700

    putchar commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*Main -Function
+* main - uses the functions declared via the arguments passed
+* Description: prints putchar
+* Return: always zero or something else passed
+*/
+
+int main(void)
+
+{
+char te[9] = "_putchar";
+int i = 0;
+for (i = 0; i < 8; i++)
+{
+
+putchar(te[i]);
+
+}
+  
+putchar('\n');
+return (0);
+}
