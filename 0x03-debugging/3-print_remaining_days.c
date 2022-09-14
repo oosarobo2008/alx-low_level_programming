9a9a6a1cf5f950f40254a33c6c29a120dac515ef
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-14 12:42:00 -0700

    debug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,7 +17,7 @@ if (month >= 3 && day >= 60)
 {
 day++;
 }
-printf("day od the year: %d\n", day);
+printf("day of the year: %d\n", day);
 printf("remaining days: %d\n", 366 - day);
 }
 else
@@ -26,10 +26,6 @@ if (month == 2 && day == 60)
 {
 printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
-else
-{
-if (month == 2 && day == 60)
-{
 else
 {
 printf("day of the year: %d\n", day);
