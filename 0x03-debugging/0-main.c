04459cabcb539cf04baab501ab3da97c5666f92d
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-14 07:21:25 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - test function that prints if int is postive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* print_remaining_days - takes a date and prints
+* left in the year
+* @month: months
+* @day: days
+* @year: year
+* Return: void
+*/
+
+void print_remaining_days(int month, int day, int year)
+{
+if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+{
+if (month >= 3 && day >= 60)
+{
+day++;
+}
+printf("day od the year: %d\n", day);
+printf("remaining days: %d\n", 366 - day);
+}
+else
+{
+if (month == 2 && day == 60)
+{
+printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+}
+else
+{
+if (month == 2 && day == 60)
+{
+else
+{
+printf("day of the year: %d\n", day);
+printf("remaining days: %d\n", 365 - day);
+}
+}
+}
