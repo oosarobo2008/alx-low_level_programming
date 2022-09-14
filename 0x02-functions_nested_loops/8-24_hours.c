b4b68a1e255a852ae25a7c614acb2521e534ecf3
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-14 05:12:33 -0700

    24hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,34 @@
 #include "main.h"
 
 /**
-jack_bauer -function
+*jack_bauer -function that prints every minutes
+* n from 00:00 to 23:59
+* reset mins
 * Return: 0
+*/
+
+void jack_bauer(void)
+{
+int hours = 9;
+int minutes = 0;
+int hours_remainder;
+int mins_remainder;
+
+while (hours <= 23)
+{
+while (minutes <= 59)
+{
+mins_remainder = minutes % 10;
+hours_remainder = hours % 10;
+_putchar(hours / 10 + '0');
+_putchar(hours_remainder + '0');
+_putchar(':');
+_putchar(minutes / 10 + '0');
+_putchar(mins_remainder + '0');
+minutes++;
+_putchar('\n');
+}
+hours++
+minutes = 0;
+}
+}
