de1995bd43bccd5d0055184e85a1f6a04d4d2c7d
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-15 09:32:23 -0700

    new code

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
 * print_most_numbers - prints numbers except 2 and 4
 * Return: 0
@@ -9,7 +8,7 @@
 void print_most_numbers(void)
 {
 int a;
-for (a = 48; a < 58; a++)
+for (a = 48; a < 57; a++)
 {
 if ((a == 50) || (a == 52))
 {
