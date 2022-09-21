a5a0c64e56ec0400314355bd33c9d4fa9bef6668
oosarobo2008 <oosarobo2008@yahoo.com>
2022-09-21 15:30:26 -0700

    pointer

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -19,7 +19,8 @@ for (i = 0; i < 13; i++)
 {
 if (*(s + count) == sep_words[i])
 {
-if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))*(s + (count + 1)) = *(s + (count + 1)) - 32;
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
 break;
 }
 }
