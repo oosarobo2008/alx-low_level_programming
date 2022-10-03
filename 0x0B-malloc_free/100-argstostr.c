9b60be2cc0c38aed8ca6f3ab012a000f70aa44cf
oosarobo2008 <oosarobo2008@yahoo.com>
2022-10-03 13:14:29 -0700

    malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,8 +9,9 @@
 
 char *argstostr(int ac, char **av)
 {
+
 char *str;
- int count = 0, a = 0, b = 0, c = 0;
+int count = 0, a = 0, b = 0, c = 0;
 if (ac == 0 || av == NULL)
 return (NULL);
 while (a < ac)
