dd1f4b6c058078ac58ecfea373945d393ca61d53
oosarobo2008 <oosarobo2008@yahoo.com>
2022-10-03 12:26:22 -0700

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,14 +16,14 @@ if (width <= 0 || height <= 0)
 {
 return (NULL);
 }
-grid - malloc(sizeof(int *) * height);
+grid = malloc(sizeof(int *) * height);
 if (grid == NULL)
 {
 return (NULL);
 }
 for (i = 0; i < height; i++)
 {
-grid[i] - malloc(sizeof(int) * width;
+grid[i] = malloc(sizeof(int) * width;
 if (grid[i] == NULL)
 {
 for (i = i - 1; i >= 0; i--)
