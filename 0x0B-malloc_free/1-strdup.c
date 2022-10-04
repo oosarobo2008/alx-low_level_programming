04b5031d7c7b9d60ae824eca881f91761aa054a7
oosarobo2008 <oosarobo2008@yahoo.com>
2022-10-04 00:57:24 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -30,6 +30,9 @@ int i = 0;
 while (src[i] != '\0')
 {
 dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
 return (dest);
 }
 
@@ -43,10 +46,12 @@ char *_strdup(char *str)
 {
 char *dst;
 unsigned int size;
+
 if (str == 0)
 {
 return (NULL);
 }
+
 size = _strlen(str) + 1;
 dst = (char *) malloc(size *sizeof(char));
 if (dst == 0)
@@ -56,4 +61,4 @@ return (NULL);
 _strcpy(dst, str);
 return (dst);
 }
-}
+
