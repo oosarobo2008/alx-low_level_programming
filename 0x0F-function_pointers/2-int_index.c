3aa55e98743c230362b888fb9f00f24e410134ff
oosarobo2008 <oosarobo2008@yahoo.com>
2022-10-12 12:35:06 -0700

    function pointer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <function_pointers.h>
+
+/**
+* int_index - reach for an integer
+* @array: array to search in
+* @size: size of the array
+* @cmp: pointer to the comparing function
+*
+* Return: index of the first element for which element for which
+* the cap function does not
+* or size is negative
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array && cmp)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This folder contains tasks on function pointers
